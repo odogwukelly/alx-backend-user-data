2c6ab429bd39fef0ecebded67c790474aad5d7e7
odogwukelly <odogwukellyice@gmail.com>
2023-11-22 09:30:30 +0000

    hot fix

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,23 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route("/unauthorized/",
-                 strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-    '''Route unauthorized requests
-
-    Returns:
-        str: 401 status code
-    '''
+    """ GET /api/v1/unauthorized
+    Return:
+      - raises a 401 error by using abort
+    """
     abort(401)
 
 
-@app_views.route("/forbidden/",
-                 strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
-    '''Route forbidden requests
-
-    Returns:
-        str: 403 status code
-    '''
+    """ GET /api/v1/forbidden
+    Return:
+      - raises a 403 error by using abort
+    """
     abort(403)
